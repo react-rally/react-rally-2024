f041dd3cc0defa1e3d8474054b29c5eab9f533df
Matt Zabriskie <mzabriskie@apple.com>
2024-08-12 15:06:25 -0600

    Update Person

--- app/components/Person.js
+++ app/components/Person.js
@@ -4,17 +4,18 @@ import Icon from "components/Icon";
 
 export default (props) => (
   <div className="Person">
-    <Avatar url={props.avatar} name={props.name} size={200} />
-    <b>{props.name}</b>
-    <br />
-    <em>{props.title}</em>
-    <div className="Person__Social">
-      {props.twitter && (
-        <Icon href={`https://twitter.com/${props.twitter}`} type="twitter" />
-      )}
-      {props.github && (
-        <Icon href={`https://github.com/${props.github}`} type="github" />
-      )}
+    <div className="Person__Wrapper">
+      <div className="Person__Social">
+        {props.twitter && (
+          <Icon href={`https://twitter.com/${props.twitter}`} type="twitter" />
+        )}
+        {props.github && (
+          <Icon href={`https://github.com/${props.github}`} type="github" />
+        )}
+      </div>
+      <Avatar url={props.avatar} name={props.name} size={200} />
+      <b>{props.name}</b>
+      <em>{props.title}</em>
     </div>
   </div>
 );
